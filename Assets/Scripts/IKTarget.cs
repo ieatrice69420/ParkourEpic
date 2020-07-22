eb45c406903c3d73d42b05f20d90c74df9f0b880
ieatrice69420 <andrewjw0106@gmail.com>
2020-07-21 21:38:34 -0500

    Multiplayer IK
    fasdfasfsafafs

---
+++ Assets/Scripts/IKTarget.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class IKTarget : MonoBehaviour
+{
+	[HideInInspector]
+	public Vector3 defaultPos;
+	
+	void Start() => defaultPos = transform.localPosition;
+	
+	public void ResetPos() => transform.localPosition = defaultPos;
+	
+	public void SetPosition(Vector3 localPos) => transform.localPosition = localPos;
+}
