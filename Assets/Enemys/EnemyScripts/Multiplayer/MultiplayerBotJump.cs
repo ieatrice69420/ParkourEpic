804cbe8ecd21c2e5561f4b77390919ed07a63ab3
ieatrice69420 <andrewjw0106@gmail.com>
2020-07-26 17:47:21 -0500

    finite state machines

---
+++ Assets/Enemys/EnemyScripts/Multiplayer/MultiplayerBotJump.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using static System.Math;
+using UnityEngine;
+using static UnityEngine.Mathf;
+using UnityEngine.AI;
+
+public class MultiplayerBotJump : MonoBehaviour
+{
+    public Transform player;
+    [SerializeField]
+    NavMeshAgent navMeshAgent;
+    [SerializeField]
+    CharacterController controller;
+    Vector3 velocity;
+    [SerializeField]
+    float jumpHeight, gravity;
+    float actualJumpHeight;
+    bool isGrounded = true;
+    Vector3 jumpDir;
+
+    void OnEnable()
+    {
+        actualJumpHeight = (float)Sqrt((double)(jumpHeight * -2f * gravity));
+        isGrounded = true;
+    }
+
+    void LateUpdate()
+    {
+        JumpCheck();
+        Move();
+        Gravity();
+    }
+
+    void Move()
+    {
+        if (isGrounded)
+        {
+            if (navMeshAgent.enabled == true && navMeshAgent.isOnNavMesh) navMeshAgent.SetDestination(player.position);
+        }
+        else
+        {
+            controller.Move(jumpDir * navMeshAgent.speed * Time.deltaTime);
+            controller.Move(velocity * Time.deltaTime);
+        }
+    }
+
+    void Gravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        if (isGrounded) velocity.y = -2f;
+    }
+
+    void JumpCheck()
+    {
+        OffMeshLinkData data = navMeshAgent.currentOffMeshLinkData;
+        if (data.valid) StartCoroutine(Jump(Vector3.Distance(data.startPos, data.endPos) / navMeshAgent.speed));
+    }
+
+    public IEnumerator Jump(float duration)
+    {
+        OffMeshLinkData data = navMeshAgent.currentOffMeshLinkData;
+        jumpDir = new Vector3((data.endPos - data.startPos).normalized.x, 0f, (data.endPos - data.startPos).normalized.z);
+        isGrounded = false;
+        navMeshAgent.enabled = false;
+        velocity.y = actualJumpHeight;
+        yield return new WaitForSeconds(duration);
+        navMeshAgent.enabled = true;
+        isGrounded = true;
+    }
+}
---
+++ Assets/Enemys/EnemyScripts/Multiplayer/MultiplayerBotStateManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class MultiplayerBotStateManager : MonoBehaviour
+{
+    [SerializeField]
+    MultiplayerBotJump multiplayerBotJump;
+
+    #region Enums
+
+	public enum MoveState
+    {
+        Jumping = 0,
+        WallRunning = 1,
+        Camping = 2
+    }
+
+    public enum PathFindState
+    {
+        Wandering = 0,
+        Following = 1,
+        Finding = 2
+    }
+
+    public enum ShootState
+    {
+        Idle = 0,
+        Spraying = 1,
+        Precise = 2
+    }
+
+    #endregion
+
+    #region States
+
+    public MoveState moveState = MoveState.Jumping;
+
+    public PathFindState pathFindState = PathFindState.Wandering;
+
+    public ShootState shootState = ShootState.Idle;
+
+    #endregion
+
+    void Update()
+    {
+        multiplayerBotJump.enabled = moveState == MoveState.Jumping;
+    }
+}
