a5df9670ae84bd24002a1124f8798aa6f8bd7724
ieatrice69420 <andrewjw0106@gmail.com>
2020-07-30 14:18:42 -0500

    kkkkkkkkkkkkkkkj

--- Assets/Scripts/WayPointManager.cs
+++ Assets/Scripts/WayPointManager.cs
@@ -5,5 +5,12 @@ public class WayPointManager : MonoBehaviour
     public static WayPointManager instance;
     public Vector3[] wayPoints;
 
-    void Awake() => instance = this;
+    void Awake() 
+    {
+        instance = this;
+        for (int i = 0; i < wayPoints.Length; i++)
+        {
+            wayPoints[i] = transform.GetChild(i).position;
+        }
+    }
 }
