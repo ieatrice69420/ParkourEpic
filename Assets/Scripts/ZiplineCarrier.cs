456cf150363a955cf5c3ad365bba9eea12d78713
ieatrice69420 <andrewjw0106@gmail.com>
2020-09-04 21:23:37 -0500

    fixed collision detect for zipline

--- Assets/Scripts/ZiplineCarrier.cs
+++ Assets/Scripts/ZiplineCarrier.cs
@@ -9,6 +9,10 @@ public class ZiplineCarrier : MonoBehaviour
     [SerializeField]
     float collisionDistance;
     public bool canBeStopped = true;
+    [SerializeField]
+    LayerMask stopperMask;
+    [SerializeField]
+    Transform stopperStart, stopperEnd;
 
     void Start() => startPos = transform.localPosition;
 
@@ -24,6 +28,13 @@ public class ZiplineCarrier : MonoBehaviour
             foreach (Transform barrier in barriers)
                 if (Vector3.SqrMagnitude(transform.position - barrier.position) <= collisionDistance * collisionDistance)
                     Stop();
+
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, stopperStart.position.x, stopperEnd.position.x),
+            transform.position.y,
+            Mathf.Clamp(transform.position.z, stopperStart.position.z, stopperEnd.position.z)
+        );
     }
 
     private void Move() => transform.Translate(velocity * Time.deltaTime);
