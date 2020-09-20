1b89082e87c90da86d3098512768e13e1594e2f2
ieatrice69420 <andrewjw0106@gmail.com>
2020-09-20 10:35:05 -0500

    a

---
+++ Assets/Scripts/WallJumpManager.cs
@@ -0,0 +1,9 @@
+﻿namespace UnityEngine.AI
+{
+    public class WallJumpManager : MonoBehaviour
+    {
+        public static WallJumpManager instance;
+        [SerializeField]
+        public Transform[] WallJumpTriggers { get; private set; }
+    }
+}
