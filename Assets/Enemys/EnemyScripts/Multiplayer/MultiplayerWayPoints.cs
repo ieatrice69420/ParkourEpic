cc83c344ba6c873c718c46982f9496fa22c0d69c
ieatrice69420 <andrewjw0106@gmail.com>
2020-09-23 11:14:38 -0500

    a

--- Assets/Enemys/EnemyScripts/Multiplayer/MultiplayerWayPoints.cs
+++ Assets/Enemys/EnemyScripts/Multiplayer/MultiplayerWayPoints.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
 
 public class MultiplayerWayPoints : MonoBehaviour
 {
---
+++ Assets/Scripts/System.Linq.Move.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace ParkourEpic.Assets.Scripts
+{
+    public class Mover
+    {
+        
+    }
+}
