d4feb929b656abb77c64266404d8e86477ea653c
ieatrice69420 <andrewjw0106@gmail.com>
2020-09-25 07:16:57 -0500

    a

---
+++ Assets/Enemys/EnemyScripts/Multiplayer/MultiplayerPlayerListAdder.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.AI;
+
+public class MultiplayerPlayerListAdder : BotClass
+{
+    MultiplayerPlayerManager manager;
+
+    private void Start()
+    {
+        manager = MultiplayerPlayerManager.instance;
+        manager.players.Add(transform);
+    }
+}
---
+++ Assets/Enemys/EnemyScripts/Multiplayer/MultiplayerPlayerManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+
+public class MultiplayerPlayerManager : BotClass
+{
+    public static MultiplayerPlayerManager instance;
+
+    public List<Transform> players = new List<Transform>();
+
+    private void Awake() => instance = this;
+}
